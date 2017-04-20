59bc495966baf5b34d58d1881394c47291d9b00b
Doug Palmer <dpalmer@chicagobooth.edu>
2017-04-19 19:05:09 -0500

    class

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,53 @@
+class GamesController < ApplicationController
+
+
+
+  def play_rock
+    @computer_move = ["rock", "paper", "scissors"].sample
+    #.sample will randomly choose one of the elements in the array
+    if @computer_move == "rock"
+      @outcome = "tie"
+    elsif @computer_move == "paper"
+      @outcome = "lose"
+    else
+      @outcome = "win"
+    end
+    render("games/play_rock.html.erb")
+  end
+
+
+
+  def play_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+    #.sample will randomly choose one of the elements in the array
+    if @computer_move == "paper"
+      @outcome = "tie"
+    elsif @computer_move == "scissors"
+      @outcome = "lose"
+    else
+      @outcome = "win"
+    end
+    render("games/play_paper.html.erb")
+  end
+
+
+
+  def play_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+    #.sample will randomly choose one of the elements in the array
+    if @computer_move == "scissors"
+      @outcome = "tie"
+    elsif @computer_move == "rock"
+      @outcome = "lose"
+    else
+      @outcome = "win"
+    end
+  render("games/play_scissors.html.erb")
+  end
+
+
+
+
+
+
+end
